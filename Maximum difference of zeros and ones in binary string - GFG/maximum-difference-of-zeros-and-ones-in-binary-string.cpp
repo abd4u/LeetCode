4316791c7fd024c0262f9c6b428c25ca05bc30b0
abd4u <109159143+abd4u@users.noreply.github.com>
2023-02-25 05:43:27 +0530

    Added solution - LeetHub

---
+++ Maximum difference of zeros and ones in binary string - GFG/maximum-difference-of-zeros-and-ones-in-binary-string.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int maxSubstring(string S)
+	{
+	    // Your code goes here
+	    int sum=0,max_diff=INT_MIN;
+		for(int i=0;i<S.length();i++){
+			if(S[i]=='1') sum+=(-1);
+			else sum+=1;
+			max_diff=max(max_diff,sum);
+			if(sum<0) sum=0;
+		}
+		return max_diff;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.maxSubstring(s) << "\n";
+   	}
+
+    return 0;
+}
+
+// } Driver Code Ends

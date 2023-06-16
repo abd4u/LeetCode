acac53c3b3e8a367a52e431cddbd6c10e7eab027
abd4u <109159143+abd4u@users.noreply.github.com>
2023-06-16 15:00:28 +0530

    Added solution - LeetHub

---
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int numberOfEnclaves(vector<vector<int>> &grid) {
+        // Code here
+        int m=grid.size(),n=grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==m-1 || j==n-1){
+                    if(grid[i][j]==1){
+                        q.push({i,j});
+                        vis[i][j]=1;
+                    }
+                }
+            }
+        }
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        int cnt=0;
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int r=row+drow[i],c=col+dcol[i];
+                if(r>=0 && r<m && c>=0 && c<n && grid[r][c]==1 && vis[r][c]==0){
+                    q.push({r,c});
+                    vis[r][c]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j]==0 && grid[i][j]==1) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.numberOfEnclaves(grid) << endl;
+    }
+}
+// } Driver Code Ends

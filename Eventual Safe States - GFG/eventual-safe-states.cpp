8e77c77f38bd918f2e24b7cac2a9ac65af983982
abd4u <109159143+abd4u@users.noreply.github.com>
2023-06-20 16:54:20 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -10,25 +10,30 @@ using namespace std;
 
 class Solution {
   public:
-    bool dfs(int s,vector<int> adj[],vector<int>& vis){
-        vis[s]=2;
-        for(auto it:adj[s]){
-            if(vis[it]==0){
-                if(dfs(it,adj,vis)) return true;
-            }
-            else if(vis[it]==2) return true;
-        }
-        vis[s]-=1;
-        return false;
-    }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
         // code here
-        vector<int> vis(V),ans;
+        vector<int> Adj[V];
+        int indeg[V]={0};
         for(int i=0;i<V;i++){
-            if(vis[i]==0){
-                if(!dfs(i,adj,vis)) ans.emplace_back(i);
-            }else if(vis[i]==1) ans.emplace_back(i);
+            indeg[i]=adj[i].size();
+            for(auto it:adj[i]){
+                Adj[it].push_back(i);
+            }
         }
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indeg[i]==0) q.push(i);
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.emplace_back(node);
+            for(auto it:Adj[node]){
+                if(--indeg[it]==0) q.push(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };

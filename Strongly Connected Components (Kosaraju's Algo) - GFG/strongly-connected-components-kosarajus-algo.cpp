0608322f45cfd44d8462af7da6d41169432b149c
abd4u <109159143+abd4u@users.noreply.github.com>
2023-07-21 18:03:09 +0530

    Added solution - LeetHub

---
+++ Strongly Connected Components (Kosaraju's Algo) - GFG/strongly-connected-components-kosarajus-algo.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	void dfs(int node,vector<vector<int>>& adj,vector<int>& vis,stack<int>& st){
+	    vis[node]=1;
+	    for(auto it:adj[node]){
+	        if(!vis[it]) dfs(it,adj,vis,st);
+	    }
+	    st.push(node);
+	}
+	void dfs1(int node,vector<vector<int>>& adj,vector<int>& vis){
+	    vis[node]=1;
+	    for(auto it:adj[node]){
+	        if(!vis[it]) dfs1(it,adj,vis);
+	    }
+	}
+	//Function to find number of strongly connected components in the graph.
+    int kosaraju(int V, vector<vector<int>>& adj)
+    {
+        //code here
+        vector<int> vis(V);
+        stack<int> st;
+        for(int i=0;i<V;i++){
+            if(!vis[i]) dfs(i,adj,vis,st);
+        }
+        vector<vector<int>> adjT(V);
+        for(int i=0;i<V;i++){
+            vis[i]=0;
+            for(auto it:adj[i]){
+                adjT[it].emplace_back(i);
+            }
+        }
+        int scc=0;
+        while(!st.empty()){
+            int node=st.top();
+            st.pop();
+            if(!vis[node]){
+                scc++;
+                dfs1(node,adjT,vis);
+            }
+        }
+        return scc;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<vector<int>> adj(V);
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.kosaraju(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
